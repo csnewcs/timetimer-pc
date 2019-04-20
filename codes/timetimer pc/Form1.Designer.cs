5f24d12841911c0cd7021f403ecb352f6ed67298
PMH Studio / CS <hj666c3@gmail.com>
2019-04-21 08:51:47 +0900

    made language setting

--- codes/timetimer pc/Form1.Designer.cs
+++ codes/timetimer pc/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(688, 8);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(104, 83);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "시작 화면";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(688, 97);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(104, 83);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "설정";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(3, 3);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(179, 15);
+            this.metroCheckBox1.TabIndex = 4;
+            this.metroCheckBox1.Text = "시계 아래에 디지털 시계 표시";
+            this.metroCheckBox1.UseSelectable = true;
+            this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroComboBox1);
+            this.metroPanel1.Controls.Add(this.metroCheckBox1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(12, 12);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(252, 89);
+            this.metroPanel1.TabIndex = 5;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            this.metroPanel1.Visible = false;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "영어 (English)",
+            "한글 (Korean)"});
+            this.metroComboBox1.Location = new System.Drawing.Point(3, 38);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(179, 29);
+            this.metroComboBox1.Sorted = true;
+            this.metroComboBox1.TabIndex = 6;
+            this.metroComboBox1.UseSelectable = true;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
     }
 }
 
--- codes/timetimer pc/Form1.cs
+++ codes/timetimer pc/Form1.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using Newtonsoft.Json.Linq;
 using System.Linq;
 using System.Text;
+using System.IO;
+using MetroFramework.Forms;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +12,62 @@ namespace timetimer_pc
 {
     public partial class Form1 : Form
     {
+        string readjson = File.ReadAllText(@"data\setting.json");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            metroPanel1.Show();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            metroPanel1.Hide();
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            JObject setting = JObject.Parse(readjson);
+            if (metroComboBox1.Text == "한글 (Korean)")
+            {
+                metroButton1.Text = "시작 화면";
+                metroButton2.Text = "설정";
+                metroCheckBox1.Text = "시계 아래에 디지털 시계 표시";
+            }
+            else if (metroComboBox1.Text == "영어 (English)")
+            {
+                metroButton1.Text = "Main page";
+                metroButton2.Text = "Setting";
+                metroCheckBox1.Text = "Show digital clock under the watch";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            JObject setting = JObject.Parse(readjson);
+            if (setting["language"].ToString() == "Korean")
+            {
+                metroButton1.Text = "시작 화면";
+                metroButton2.Text = "설정";
+                metroCheckBox1.Text = "시계 아래에 디지털 시계 표시";
+                metroComboBox1.Text = "한글 (Korean)";
+            }
+            else if (setting["language"].ToString() == "English")
+            {
+                metroButton1.Text = "Main page";
+                metroButton2.Text = "Setting";
+                metroCheckBox1.Text = "Show digital clock under the watch";
+                metroComboBox1.Text = "영어 (English)";
+            }
         }
     }
 }
